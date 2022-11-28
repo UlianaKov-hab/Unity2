23098b261551ca7dbd741f345f10842b6d97fb3f
Uliana <a_k_ula@ukr.net>
2022-11-28 19:34:13 +0200

    game wirker2

--- My project (1)/Assets/Scripts/FollowCam.cs
+++ My project (1)/Assets/Scripts/FollowCam.cs
@@ -19,10 +19,45 @@ public class FollowCam : MonoBehaviour
 
     public void FixedUpdate()
     {
-        if (POI == null) return;  // выйти, если нет интересующего объекта
+       /* if (POI == null) return;  // выйти, если нет интересующего объекта
 
         // Получить позицию интересующего объекта
-        Vector3 destination = POI.transform.position;
+        Vector3 destination = POI.transform.position;*/
+        Vector3 destination;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+
+            if (POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
+
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Принудительно установить значение destination.z равным camZ, чтобы
+        // отодвинуть камеру подальше
+        destination.z = camZ;
+        // Поместить камеру в позицию destination
+        transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
+
+
+
+
         // Ограничить X и Y минимальными значениями
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
--- My project (1)/Assets/Scripts/Slingshot.cs
+++ My project (1)/Assets/Scripts/Slingshot.cs
@@ -4,79 +4,90 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
+    // Поля, устанавливаемые в инспекторе Unity
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
 
-    [Header("Set Ynamically")]
+    // Поля, устанавливаемые динамически
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
-    public bool aimingMode;
+    public bool aimingMood;
     private Rigidbody projectileRigidbody;
 
-    private void Awake()
+    static public Vector3 LAUNCH_POS
     {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+    void Awake()
+    {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
         launchPos = launchPointTrans.position;
     }
-    private void OnMouseEnter()
+    void OnMouseEnter()
     {
-        /*print("Slingshot: OnMouseEnter()");*/
+        //print("Slingshot:OnMouseEnter()");
         launchPoint.SetActive(true);
     }
-
-    private void OnMouseExit()
+    void OnMouseExit()
     {
-        /*print("Slingshot: OnMouseExit()");*/
+        //print("Slingshot:OnMouseExit()");
         launchPoint.SetActive(false);
     }
-
-    private void OnMouseDown()
+    void OnMouseDown()
     {
-        aimingMode = true;
+        aimingMood = true;
         projectile = Instantiate(prefabProjectile) as GameObject;
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
 
+        // Сделать его кинематическим
         projectileRigidbody = projectile.GetComponent<Rigidbody>();
         projectileRigidbody.isKinematic = true;
-
     }
-
-    private void Update()
+    void Update()
     {
-        // Если рогатка не в режиме прицеливания, не выполнять этот код
-        if (!aimingMode) return;
+        // если рогатка не в режиме прицеливанияб не выполнять этот код
+        if (!aimingMood) return;
+
+        // Получить текущие экранные кор. указателя мыши
         Vector3 mousePos2D = Input.mousePosition;
         mousePos2D.z = -Camera.main.transform.position.z;
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
-        // Найти разность координат между launchPos и mousePos3D
+        // Найти разность коорд. между launchPos и mousePos3D
         Vector3 mouseDelta = mousePos3D - launchPos;
-        // Ограничить mouseDelta радиусом коллайдера объекта Slingshot
+
+        // Ограничить mouseDelta радиусом коллайдера обьекта SLINGSHOT 
         float maxMagnitude = this.GetComponent<SphereCollider>().radius;
         if (mouseDelta.magnitude > maxMagnitude)
         {
             mouseDelta.Normalize();
             mouseDelta *= maxMagnitude;
-
-
         }
+
         // Передвинуть снаряд в новую позицию
         Vector3 projPos = launchPos + mouseDelta;
         projectile.transform.position = projPos;
         if (Input.GetMouseButtonUp(0))
-        { 
-          // Кнопка мыши отпущена
-            aimingMode = false;
+        {
+            // Кнопка мыщи отпущена
+            aimingMood = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
         }
-
     }
 }
+
