13ad7ad862a96fdc352cae47aec34d2ba102546b
Uliana <a_k_ula@ukr.net>
2022-12-02 20:26:32 +0200

    working 4

--- My project (1)/Assets/Scripts/Goal.cs
+++ My project (1)/Assets/Scripts/Goal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
@@ -17,6 +18,10 @@ public class Goal : MonoBehaviour
             c.a = 1;
 
             mat.color = c;
+            if (mat.color == c)
+            {
+                SceneManager.LoadScene("_Scene_0");
+            }
         }
     }
 }
